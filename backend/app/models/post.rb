2521601c957792c74e9ac43f8af600e0980c60a5
kousei1015 <wtks39176@gmail.com>
2023-02-23 15:53:31 +0900

    added validation title column of Post mdoel

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   has_many :tags, through: :taggings
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
+  validates :title, presence: true
 end
